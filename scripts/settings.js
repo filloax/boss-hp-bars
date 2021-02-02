83cd34d49044de0c22b55289a1100c897621f2b1
filloax <filloax@gmail.com>
2021-02-03 00:39:32 +0100

    minor fix

--- scripts/settings.js
+++ scripts/settings.js
@@ -6,7 +6,7 @@ export const registerSettings = function () {
     game.settings.register(Constants.MOD_NAME, "debug-enabled", {
         name: localize("settings.debug"),
         // hint: localize("settings.debug"),
-        scope: "user",
+        scope: "client",
         config: true,
         default: true,
         type: Boolean,
